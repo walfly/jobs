3db23f42bd8885cff0c7714518fd60552b797d16
walfly <walker.d.flynn@gmail.com>
2014-08-21 13:18:14 -0400

    initial grunt set up

---
+++ app/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    karma: {
+      unit: {
+        configFile: './karma.conf.js'
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+      options: {
+        reporter: require('jshint-stylish')
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          './public/stylesheets/main.css': './public/stylesheets/main.scss',
+        }
+      }
+    },
+    browserify: {
+      js: {
+        // A single entry point for our app
+        src: './public/javascripts/app.js',
+        // Compile to a single file to add a script tag for in your HTML
+        dest: './public/javascripts/bundle.js'
+      },
+      spec: {
+        src: './public/javascripts/**/*.test.js',
+        dest: './public/javascripts/specs.js'
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['jshint', 'browserify', 'karma', 'sass']);
+};
