09d06e07a3f7ae4566ff1c3ab86a11d11fb77d42
walfly <walker.d.flynn@gmail.com>
2014-08-22 17:47:16 -0400

    temaplates!!!!!!!! I hate myself

--- app/public/javascripts/addEventButton/controller.js
+++ app/public/javascripts/addEventButton/controller.js
@@ -1,9 +1,10 @@
 module.exports = function ($scope, calendarData) {
   $scope.addEvent = function () {
-    var event = {
+    var obj = {
       startTime: new Date(2014, 7, 20, 10),
       endTime: new Date(2014, 7, 20, 12),
-    }
-    calendarData.addEvent(event);
+    };
+    console.log(obj);
+    calendarData.addEvent(obj);
   };
 };
--- app/public/javascripts/week/eventDirective.js
+++ app/public/javascripts/week/eventDirective.js
@@ -5,16 +5,20 @@ module.exports = function (timePosition) {
     scope: {
       event: '=event'
     },
-    temaplateUrl: '/angularTemplates/eventTemplate.html',
+    templateUrl: '/angularTemplates/eventTemplate.html',
     link: function (scope, element, attr) {
-      var position = timePosition.startAndEnd(element.parent(), scope.event.startTime, scope.event.endTime);
-      var width = element.parent().width()/scope.event.overlaps;
-      element.css({
-        top: position.start,
-        height: position.end - position.start,
-        left: scope.event.overlapPos * width,
-        width: width
-      });
+      var buildDiv = function () {
+        var position = timePosition.startAndEnd(element.parent(), scope.event.startTime, scope.event.endTime);
+        console.log('hey', position);
+        var width = element.parent().width()/(scope.event.overlaps + 1);
+        element.css({
+          top: position.start,
+          height: position.end - position.start,
+          left: scope.event.overlapPos * width,
+          width: width
+        });
+      };
+      scope.$watch('event.overlaps', buildDiv);
     }
   };
 };
