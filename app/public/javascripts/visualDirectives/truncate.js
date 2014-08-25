d6d0fa1030b237512632de20edb2ae76d0e42fac
walfly <walker.d.flynn@gmail.com>
2014-08-25 12:42:31 -0400

    alternat overlap handling

--- app/public/javascripts/visualDirectives/truncate.js
+++ app/public/javascripts/visualDirectives/truncate.js
@@ -3,41 +3,58 @@ module.exports = function ($timeout) {
     restrict: 'A',
     link: function (scope, element, attr) {
       var isTruncated = false;
+      var text;
+      var newText;
       var update = function () {
         var parent = element.parent();
-        var lineHeight = element.css('font-size').split('p')[0];
-        if(element.height() > (parent.height() - Number(lineHeight))){
+        var fontSize = Number(element.css('font-size').split('p')[0]);
+        var heightDifference = element.height() - (parent.height() - fontSize);
+        if (heightDifference > 0) {
+          console.log('tuncating');
           parent.addClass('truncated');
+          // calculate how much to trim
+          var approxLines = element.height()/fontSize;
+          var overLapLines = heightDifference/fontSize;
+          var truncatePercent = overLapLines/approxLines;
+          newText = text.slice(0, text.length - Math.floor(text.length * truncatePercent));
+          element.text(newText + '...');
+          // set the truncated flag
           isTruncated = true;
         } else {
           parent.removeClass('truncated');
+          // set the text back if changed
+          element.text(text);
           isTruncated = false;
         }
       };
       var bindTruncationEvents = function () {
-        var eventHeight = element.parent().height();
-        element.on('mouseenter', function () {
-          var parent = element.parent();
-          parent.css({
-            'height': 'auto',
-            'z-index': 1000
-          });
+        var parent = element.parent();
+        var eventHeight = parent.height();
+        parent.on('mouseenter', function () {
+          parent.css('height', 'auto');
+          element.text(text);
           parent.removeClass('truncated'); 
         });
-        element.on('mouseleave', function () {
+        parent.on('mouseleave', function () {
           var parent = element.parent();
+          element.text(newText + '...');
           parent.height(eventHeight);
           parent.addClass('truncated');
         });
       };
       var unbindTruncationEvents = function () {
-        element.unbind('mouseenter');
-        element.unbind('mouseleave');
+        var parent = element.parent();
+        parent.unbind('mouseenter');
+        parent.unbind('mouseleave');
       };
       var timeUpdate = function () {
         // wait for dom repaint
         // kind of a hack
+        // set text only on first run
         setTimeout(function () {
+          if (!text) {
+            text = element.text();
+          }
           update();
           if(isTruncated){
             bindTruncationEvents();
--- app/public/javascripts/week/eventDirective.js
+++ app/public/javascripts/week/eventDirective.js
@@ -9,13 +9,17 @@ module.exports = function (timePosition) {
     link: function (scope, element, attr) {
       var update = function () {
         var position = timePosition.startAndEnd(element.parent(), scope.event.startTime, scope.event.endTime);
-        var width = element.parent().width()/(scope.event.overlaps + 1);
+        var offset = element.parent().width()/10;
+        var width = element.parent().width() - (scope.event.overlaps * offset);
         element.css({
-          top: position.start,
-          height: position.end - position.start,
-          left: scope.event.overlapPos * width,
-          width: width
+          'top': position.start,
+          'height': position.end - position.start,
+          'left': scope.event.overlapPos * offset,
+          'width': width
         });
+        if (scope.event.overlapPos > 0) {
+          element.addClass('overlapping');
+        }
       };
       scope.$watch('event.overlaps', update);
     }
